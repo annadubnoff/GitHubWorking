3f063a895f50cb0cd7e978692722121b30ac34c6
Anna Dubnoff <annadubnoff@gmail.com>
2020-02-03 13:59:48 -0500

    Create Alien Two

--- GitHubWorking/GameScene.swift
+++ GitHubWorking/GameScene.swift
@@ -20,6 +20,9 @@ class GameScene: SKScene {
         player1.zPosition = 1.0
         addChild(player1)
         
-
+        let player2 = SKSpriteNode(imageNamed: "Player2")
+        player2.position = CGPoint(x: 0, y: 100)
+        player2.zPosition = 1.0
+        addChild(player2)
     }
 }
