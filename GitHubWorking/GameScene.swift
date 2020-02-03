6d36ae4e8168c03cd553561d7e8a44d3acf59d61
Anna Dubnoff <annadubnoff@gmail.com>
2020-02-03 13:55:04 -0500

    Deleted Player Two

--- GitHubWorking/GameScene.swift
+++ GitHubWorking/GameScene.swift
@@ -20,9 +20,6 @@ class GameScene: SKScene {
         player1.zPosition = 1.0
         addChild(player1)
         
-        let player2 = SKSpriteNode(imageNamed: "Player2")
-        player2.position = CGPoint(x: 0, y: 100)
-        player2.zPosition = 1.0
-        addChild(player2)
+
     }
 }
